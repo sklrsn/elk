d970bbff2aa94a3378d9bc390e285ff25cebc43e
Kalai <skl.rsn@gmail.com>
2021-10-09 20:58:10 +0300

    Add rabbitmq

---
+++ bootstrap/rabbitmq.go
@@ -0,0 +1,78 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/streadway/amqp"
+)
+
+const (
+	dlExchange       = "elk-dead-letter"
+	dlQueue          = "elk-dead-letter-queue"
+	unroutedExchange = "elk-unrouted"
+	elkExchange      = "elk-exchange"
+	elkQueue         = "elk-queue"
+	unroutedQueue    = "elk-unrouted-queue"
+)
+
+func init() {
+
+}
+
+func main() {
+	conn, err := amqp.Dial("amqp://guest:guest@rabbitmq:5672/")
+	handleErr(err)
+	defer conn.Close()
+
+	// 1. DLQ
+	dqchan, err := conn.Channel()
+	handleErr(err)
+
+	err = dqchan.ExchangeDeclare(dlExchange, "direct", true, false, false, false, nil)
+	handleErr(err)
+
+	dq, err := dqchan.QueueDeclare(dlQueue, true, false, false, false, nil)
+	handleErr(err)
+
+	err = dqchan.QueueBind(dq.Name, "#", dlExchange, false, nil)
+	handleErr(err)
+
+	// 2. ALT
+	achan, err := conn.Channel()
+	handleErr(err)
+
+	err = achan.ExchangeDeclare(unroutedExchange, "direct", true, false, false, false, nil)
+	handleErr(err)
+
+	aq, err := achan.QueueDeclare(unroutedQueue, true, false, false, false, nil)
+	handleErr(err)
+
+	err = achan.QueueBind(aq.Name, "#", unroutedExchange, false, nil)
+	handleErr(err)
+
+	// 3. ELK
+	rqchan, err := conn.Channel()
+	handleErr(err)
+
+	err = rqchan.ExchangeDeclare(elkExchange, "x-consistent-hash", true, false, false, false,
+		amqp.Table{"alternate-exchange": unroutedExchange})
+	handleErr(err)
+
+	for qID := 1; qID <= 2; qID++ {
+		qName := fmt.Sprintf("%v-0%v", elkQueue, qID)
+		q, err := rqchan.QueueDeclare(qName, true, false, false, false,
+			amqp.Table{"x-max-priority": uint8(10), "x-dead-letter-exchange": dlExchange})
+		handleErr(err)
+
+		err = rqchan.QueueBind(q.Name, "10", elkExchange, false, nil)
+		handleErr(err)
+	}
+}
+
+func handleErr(err error) {
+	if err != nil {
+		log.Println(err)
+		log.Fatalln(err)
+	}
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sklrsn/elk
 
 go 1.17
+
+require github.com/streadway/amqp v1.0.0
